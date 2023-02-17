eb55b9fe4434cc530345e52084e4a9d853c5bcbc
pottava <pottava@gmail.com>
2023-02-17 10:00:14 +0900

    [ci skip] Revise test scripts

--- deploy/test.sh
+++ deploy/test.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-set -e
-
 cd src
 pip install poetry
-poetry install --no-interaction --no-ansi
+poetry install --no-interaction --no-ansi --no-root
+
+set -e
 poetry run isort .
 poetry run black .
 poetry run flake8 --exclude=.venv .
